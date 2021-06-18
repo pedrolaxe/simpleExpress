4ccc75fefdfc6e315bf74e6a5801eb6ffa101ee9
Pedro Laxe <pedrolaxe@gmail.com>
2021-06-18 00:28:47 -0300

    Adding Update to CRUD

--- src/app.js
+++ src/app.js
@@ -12,18 +12,51 @@ const con = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+//Main Route
 app.get('/', (req, res) =>{
     res.json({'status': 'ok'});
 });
 
-//Select
+//SELECT (GET)
 app.get('/users', (req, res) =>{
     con.query('SELECT * FROM users', (err,rows) => {
         if(err) throw err;
         return res.json(rows);
     });
 });
-//Add (PUT)
+
+//UPDATE (POST)
+app.post('/users', (req, res) =>{
+    con.query("SELECT id FROM users WHERE id='"+req.body.id+"'", (err,rows) => {
+        if(err) throw err;
+        
+        const data = {
+            id: req.body.id,
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        }
+        
+        data.password = crypto.createHash('sha1').update(req.body.password).digest('hex')
+        
+        if (rows.length > 0 && data.id && data.name && data.username && data.password) {
+            var sql = "UPDATE users SET name = '"+data.name+"', username = '"+data.username+"', password = '"+data.password+"' WHERE id = '"+data.id+"'";
+            con.query(sql, function (err, result) {
+                  if (err) throw err;
+                    if(result.affectedRows > 0){
+                        return res.json({'update': 'ok'});
+                    }else{
+                        return res.json({'update': 'fail'});
+                    }
+                }
+              ); 
+        }else{
+            return res.json({'update': 'ID not found'});
+        }
+    });
+});
+
+//CREATE (PUT)
 app.put('/users', function (req, res) {
     // crypt password to sha1  
     let pass = crypto.createHash('sha1').update(req.body.password).digest('hex')
@@ -46,7 +79,7 @@ app.put('/users', function (req, res) {
         }
     });
 })
-// Delete
+// DELETE (DELETE)
 app.delete('/users', function (req, res) {
     const data = {
         id: req.body.id
@@ -63,6 +96,5 @@ app.delete('/users', function (req, res) {
     );
     
 })
-
 
 app.listen(3000)
